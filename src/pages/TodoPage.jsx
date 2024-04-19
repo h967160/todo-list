c1e258bd801ed60f2e71a0397b4b316078548f40
Huang <h96716@gmail.com>
2024-04-19 20:45:14 +0800

    feat: add current member at todo page

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -8,7 +8,7 @@ const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, currentMember } = useAuth();
 
   const handleChange = (value) => {
     setInputValue(value);
@@ -162,7 +162,7 @@ const TodoPage = () => {
   return (
     <div>
       TodoPage
-      <Header />
+      <Header username={currentMember?.name} />
       <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
