b4f867fb35e8b6e48c9518309d3ada76fa6d1873
Huang <h96716@gmail.com>
2024-04-21 16:15:06 +0800

    refactor: todo.js to use axios interceptors for authentication token

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
 
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todos failed]: ', error);
   }
@@ -13,8 +30,9 @@ export const getTodos = async () => {
 
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
+
   try {
-    const res = await axios.post(`${baseUrl}/todos`, {
+    const res = await axiosInstance.post(`${baseUrl}/todos`, {
       title,
       isDone,
     });
@@ -25,23 +43,24 @@ export const createTodo = async (payload) => {
 };
 
 export const patchTodo = async (payload) => {
+  const { id, title, isDone } = payload;
   try {
-    const { id, title, isDone } = payload;
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
       title,
       isDone,
     });
+
     return res.data;
   } catch (error) {
-    console.error('[Petch Todo failed]: ', error);
+    console.error('[Patch Todo failed]:', error);
   }
 };
 
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data;
   } catch (error) {
-    console.error('[Delete Todo failed]: ', error);
+    console.error('[Delete Todo failed]:', error);
   }
 };
